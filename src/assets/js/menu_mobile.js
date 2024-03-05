e79665de6df48ec4adeb64e9e001be8a533ce6e9
han19githtbr <milliance23@gmail.com>
2024-03-05 16:31:03 -0300

    update

--- src/assets/js/menu_mobile.js
+++ src/assets/js/menu_mobile.js
@@ -6,18 +6,39 @@ $(function(){
   $('nav.menu_item i').click(function(){
     var el = $(this).parent().find('ul');
     if (el.is(':visible') == false) {
-      //$('.conteudo-header').css('display','none');
       el.fadeIn();
-      $('nav.menu_item .btn').click(function(){
-        $('.conteudo-header .menu_item').css('display','none');
-        $('.login-form').fadeIn();
-        el.fadeOut();
-      })
-      $('.login-form').fadeOut();
-      $('.conteudo-header').css('display','block');
-    }else{
+    } else {
       el.fadeOut();
-      $('.login-form').fadeOut();
+    }
+  });
+
+  // Esconder o menu ao carregar a página em dispositivos móveis
+  if ($(window).width() <= 768) {
+    $('nav.menu_item ul').hide();
   }
 });
+
+
+
+$(document).ready(function(){
+  $('.exibir-mais').click(function(){
+      $(this).prev('p').css('max-height', 'none'); // Remove a altura máxima
+      $(this).hide(); // Esconde o botão "Exibir mais"
+      $(this).next('.exibir-menos').show(); // Exibe o botão "Exibir menos"
+  });
+
+  $('.exibir-menos').click(function(){
+      $(this).prev().prev('p').css('max-height', '50px'); // Restaura a altura máxima
+      $(this).hide(); // Esconde o botão "Exibir menos"
+      $(this).prev('.exibir-mais').show(); // Exibe o botão "Exibir mais"
+  });
+
+  // Verifica se o texto precisa ser truncado inicialmente
+  $('p').each(function(){
+    if ($(this).height() > 50) { // Verifica se o texto ultrapassa a altura máxima
+      $(this).css('max-height', '50px'); // Trunca o texto
+      $(this).next('.exibir-mais').show(); // Exibe o botão "Exibir mais"
+    }
+  });
 });
+
